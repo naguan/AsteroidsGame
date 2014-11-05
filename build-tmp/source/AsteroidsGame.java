183d4aa0b04ca0da64f0dae06e7266cd0e8f692d
unknown <naguan@697-334-Forlond.sf.k12.internal>
2014-11-05 10:19:39 -0800

    Asteroids.html

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 private SpaceShip steve;
 private Stars[] joe;
 public void setup() 
@@ -32,15 +48,15 @@ public void draw()
     }
     if (key == 'w')
     {
-      steve.accelerate(0.1);
+      steve.accelerate(0.1f);
     }
     if (key == 's')
     {
-      steve.accelerate(-0.1);
+      steve.accelerate(-0.1f);
     }
   }
 }  
-    void keyPressed() {
+    public void keyPressed() {
       if (key == 'z')
       {
 
@@ -193,3 +209,12 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
     endShape(CLOSE);
   }
 } 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
